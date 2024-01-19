79de1d4f2738c37d13b37229b398397b23026150
daviswest <90291673+daviswest@users.noreply.github.com>
2024-01-19 11:26:53 -0500

    welcome, login, and register

---
+++ Capstone App/Capstone App/Capstone_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_AppApp.swift
+//  Capstone App
+//
+//  Created by Davis West on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
