fb1a5ee1b203c38f9ea6a47a852a8c22c6b812e5
daviswest <90291673+daviswest@users.noreply.github.com>
2024-02-07 13:09:59 -0500

    Spacing and scrolling for login and register

--- Capstone App/Capstone App/RegisterView.swift
+++ Capstone App/Capstone App/RegisterView.swift
@@ -11,46 +11,41 @@ struct RegisterView: View {
     @State private var confirmPassword: String = ""
     @State private var zipCode: String = ""
     @State private var errorMessage: String?
-    
+
     var body: some View {
-        VStack(spacing: 20) {
+        ScrollView {
             Text("We're glad you're here")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .padding(.top, 60)
-                
+            
             Text("Please fill out some details to register")
                 .font(.headline)
                 .fontWeight(.medium)
                 .foregroundColor(.secondary)
                 .padding(.bottom, 20)
             
-            Group {
-                TextField("First Name", text: $firstName)
-                TextField("Last Name", text: $lastName)
-                TextField("Email", text: $email)
-                    .autocapitalization(.none)
-                            
-                PasswordField(placeholder: "Password", password: $password)
-                PasswordField(placeholder: "Confirm Password", password: $confirmPassword)
-                            
-                TextField("ZIP Code", text: $zipCode)
-                    .keyboardType(.numberPad)
+            VStack(spacing: 20) {
+                Group {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                    
+                    PasswordField(placeholder: "Password", password: $password)
+                    PasswordField(placeholder: "Confirm Password", password: $confirmPassword)
+                    
+                    TextField("ZIP Code", text: $zipCode)
+                        .keyboardType(.numberPad)
                 }
                 .padding()
                 .background(Color(.systemGray6))
                 .cornerRadius(10)
                 .padding(.horizontal, 20)
-            
-            Button(action: performRegistration) {
-                Text("REGISTER")
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
             }
-            .padding(.horizontal, 20)
+        }
+            
             if let errorMessage = errorMessage {
                 Text(errorMessage)
                     .font(.caption)
@@ -60,8 +55,18 @@ struct RegisterView: View {
                     .cornerRadius(5)
                     .padding(.bottom, 10)
             }
+            
+            Button(action: performRegistration) {
+                Text("REGISTER")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(Color.black)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 20)
+            
             Spacer()
-        }
         .navigationBarTitle("", displayMode: .inline)
         .navigationBarBackButtonHidden(false)
     }
